9d744f41dc5ca87143ba7d97ec5294b8b6547853
Davor Heredia <davorthebest@gmail.com>
2018-11-29 22:28:30 -0400

    Buildeando proyecto

--- Astrix/app/src/main/java/com/android/software/astrix/views/ViewVideo.java
+++ Astrix/app/src/main/java/com/android/software/astrix/views/ViewVideo.java
@@ -3,6 +3,7 @@ package com.android.software.astrix.views;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -16,12 +17,12 @@ public class ViewVideo extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.view_video);
         playVideo();
 
     }
     private void playVideo(){
-        VideoView videoView =(VideoView)findViewById(R.id.videoView);
+        WebView videoView =(WebView) findViewById(R.id.videoView);
         MediaController mediaController= new MediaController(this);
         mediaController.setAnchorView(videoView);
         Uri uri=Uri.parse("https://youtu.be/bSMZknDI6bg");
