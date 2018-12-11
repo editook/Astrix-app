3ca48d78b97ab55d3824918b01079ccf2e7526ac
eduardo perez <edito.ok@gmail.com>
2018-12-11 03:05:22 +0100

    .

--- Astrix/app/src/main/java/com/android/software/astrix/App.java
+++ Astrix/app/src/main/java/com/android/software/astrix/App.java
@@ -1,6 +1,8 @@
 package com.android.software.astrix;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +10,59 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.android.software.astrix.views.StartApp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App extends AppCompatActivity {
 
+    VideoView videoView;
+    ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList("https://mega.nz/#!XpZF3S7R!7iZOqcTxcvMzTvtMHKJ79C732R_shy8CVWZZTOR_sp4","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
 
+    int index = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        final MediaController mediacontroller = new MediaController(this);
+        mediacontroller.setAnchorView(videoView);
+
+
+        videoView.setMediaController(mediacontroller);
+        videoView.setVideoURI(Uri.parse(arrayList.get(index)));
+        videoView.requestFocus();
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(getApplicationContext(), "Video over", Toast.LENGTH_SHORT).show();
+                if (index++ == arrayList.size()) {
+                    index = 0;
+                    mp.release();
+                    Toast.makeText(getApplicationContext(), "Video over", Toast.LENGTH_SHORT).show();
+                } else {
+                    videoView.setVideoURI(Uri.parse(arrayList.get(index)));
+                    videoView.start();
+                }
+
+
+            }
+        });
+
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Log.d("API123", "What " + what + " extra " + extra);
+                return false;
+            }
+        });
+    }
 }
