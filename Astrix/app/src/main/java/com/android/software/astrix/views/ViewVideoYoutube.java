b22b17a4aa88a2ba911997fa492a101d30d308fb
Davor Heredia <davorthebest@gmail.com>
2018-12-17 01:12:59 -0400

    Sorting videos

--- Astrix/app/src/main/java/com/android/software/astrix/views/ViewVideoYoutube.java
+++ Astrix/app/src/main/java/com/android/software/astrix/views/ViewVideoYoutube.java
@@ -18,10 +18,10 @@ import java.util.Arrays;
 public class ViewVideoYoutube extends AppCompatActivity {
     private static final ArrayList<String> arrayList = new ArrayList<String>(
             Arrays.asList("https://www.youtube.com/embed/dRwq9rAZGyA",
-                    "https://www.youtube.com/embed/oeYKN6dFOcA",
-                    "https://www.youtube.com/embed/5V6h1i-7I4s",
-                    "https://www.youtube.com/embed/6Xs6-j2DIdY",
-                    "https://www.youtube.com/embed/eVzVxS5Pe9A"));
+                        "https://www.youtube.com/embed/5V6h1i-7I4s",
+                        "https://www.youtube.com/embed/oeYKN6dFOcA",
+                        "https://www.youtube.com/embed/6Xs6-j2DIdY",
+                        "https://www.youtube.com/embed/eVzVxS5Pe9A"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
